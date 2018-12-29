fab39da1de8c89118f67428800d2e6e9e6b4d7cd
MeiK <meik2333@gmail.com>
2018-12-29 11:15:31 +0800

    填错一个参数。。。

--- AcFun/AcFun/spiders/danmu.py
+++ AcFun/AcFun/spiders/danmu.py
@@ -19,8 +19,8 @@ class DanmuSpider(RedisCrawlSpider):
     def start_requests(self):
         ac_start = os.environ.get('ac_start', '4166948')
         ac_end = os.environ.get('ac_end', '4819800')  # 截至我写代码时的最后一个
-        for i in range(int(ac_start), int(ac_end) + 1):
-            yield scrapy.Request(f'http://www.acfun.cn/v/ac{i}', callback=self.parse_page)
+        for page_id in range(int(ac_start), int(ac_end) + 1):
+            yield scrapy.Request(f'http://www.acfun.cn/v/ac{page_id}', callback=self.parse_page, meta={'page_id': page_id})
 
     def parse_page(self, response):
         # 重定向不做处理
@@ -36,15 +36,15 @@ class DanmuSpider(RedisCrawlSpider):
 
         for video in data['videoList']:
             # 仅获取 2 类型的弹幕（高级弹幕也很难分析）
-            yield scrapy.Request(f'http://danmu.aixifan.com/V4/{video["source_id"]}_2/0/1000?order=1', meta={'vid': video['source_id']})
+            yield scrapy.Request(f'http://danmu.aixifan.com/V4/{video["id"]}_2/0/1000?order=1', meta={'video_id': video['id']})
 
     def parse(self, response):
         data = json.loads(response.body_as_unicode())[2]
         max_time = 0
-        vid = response.meta['vid']
+        video_id = response.meta['video_id']
 
         for i in data:
-            i['vid'] = response.meta['vid']
+            i['video_id'] = response.meta['video_id']
             i['c'] = i['c'].split(',')
             i['c'][5] = int(i['c'][5])
             max_time = max(max_time, i['c'][5])
@@ -56,4 +56,4 @@ class DanmuSpider(RedisCrawlSpider):
 
         # 如果本页有 1000 条数据，则尝试获取下一页
         if len(data) == 1000:
-            yield scrapy.Request(f'http://danmu.aixifan.com/V4/{vid}_2/{max_time}/1000?order=1', meta={'vid': vid})
+            yield scrapy.Request(f'http://danmu.aixifan.com/V4/{video_id}_2/{max_time}/1000?order=1', meta={'video_id': video_id})
